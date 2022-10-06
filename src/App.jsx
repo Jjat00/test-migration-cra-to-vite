40ac4f5f0e63e07f2aaf35503a58feb1a4c13826
Jaime Aza <jaime.aza@decimetrix.com>
2022-10-06 13:38:01 -0500

    Add connection with backend

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+import useFetch from "./hooks/useFetch";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const [data, error] = useFetch("/api/v1/objects");
+
+  if (error) return <p>Errot get objects</p>;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Counter: <span>{counter}</span>
+        </p>
+        <div>
+          <button onClick={() => setCounter(counter + 1)}>+</button>
+          <button onClick={() => setCounter(counter - 1)}>-</button>
+        </div>
+        <div className="container-locations"> 
+          {data &&
+            data.map((item) => (
+              <div>
+                <p>{item.id}</p>
+                <p>latitude: {item.location.latitude}</p>
+                <p>longitude: {item.location.longitude}</p>
+              </div>
+            ))}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const useGet = (endpoint) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const token = process.env.REACT_APP_TOKEN;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_URL_BACKEND}${endpoint}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+              Authorization: token ? `Bearer ${token}` : "",
+            },
+          }
+        );
+        const json = await response.json();
+        if (json.statusCode === 401) throw new Error(json.error);
+        setData(json);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, [endpoint, token]);
+
+  return [data, error, setData];
+};
+
+export default useGet;
